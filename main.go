2c5f96091427ee4feaffa3e941e0155c8afa90e9
neil <esz759486@gmail.com>
2019-03-17 16:10:56 +0800

    gin

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		c.Next()
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(CORSMiddleware())
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080
+}
